fcc8263408224cd61ed7ffa8d2c3badda730ae69
MatveyVi <137712857+MatveyVi@users.noreply.github.com>
2023-06-26 18:49:28 +0300

    PlayerMovement
    Мумент в мэин ветке

---
+++ Brodilka/Assets/Matvey/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    private bool flipRight = true;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if (move > 0 && !flipRight)
+        {
+            Flip();
+        }
+        if (move < 0 && flipRight)
+        {
+            Flip();
+        }
+
+    }
+    private void Flip()
+    {
+        flipRight = !flipRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
+
+
