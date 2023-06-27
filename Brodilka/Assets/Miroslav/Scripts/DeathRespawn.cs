d3a83b8478416be7ea7398b22c98b5a46bf38469
MatveyVi <137712857+MatveyVi@users.noreply.github.com>
2023-06-27 23:49:25 +0300

    CameraShaking

--- Brodilka/Assets/Miroslav/Scripts/DeathRespawn.cs
+++ Brodilka/Assets/Miroslav/Scripts/DeathRespawn.cs
@@ -8,13 +8,19 @@ public class DeathRespawn : MonoBehaviour
     public GameObject EffectDie;
     public GameObject EffectAlive;
     public GameObject GrobRip;
+    private Animator camAnim;
 
     public float RespawnDelay;
+    private void Start()
+    {
+        camAnim = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Killing"))
         {
             Instantiate(EffectDie, transform.position, Quaternion.identity);
+            camAnim.SetTrigger("Shake");
             Player.SetActive(false);
             Instantiate(GrobRip, transform.position, Quaternion.Euler(0, 0, Random.Range(0f, 360f)));
             Player.transform.position = new Vector2(-11.5f, -5f);
@@ -24,6 +30,7 @@ public class DeathRespawn : MonoBehaviour
     private void Respawn()
     {
         Player.SetActive(true);
+        camAnim.SetTrigger("Shake");
         Instantiate(EffectAlive, transform.position, Quaternion.identity);
     }
 }
