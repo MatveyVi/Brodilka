7c8ffda2cfac2384fabd1d69e52c6fd71d22d7af
MatveyVi <137712857+MatveyVi@users.noreply.github.com>
2023-06-28 13:08:41 +0300

    Переход меж уровней
    !не говнокод

--- Brodilka/Assets/Miroslav/Scripts/LevelTransition.cs
+++ Brodilka/Assets/Miroslav/Scripts/LevelTransition.cs
@@ -5,27 +5,41 @@ public class LevelTransition : MonoBehaviour
 {
     private string sceneName;
     private int levelToLoad;
-    
+    private GameObject FinishObj;
+    // private int levelToLoad;
+
 
     private void Start()
     {
-        sceneName = SceneManager.GetActiveScene().name;
-        var level = ((sceneName)[5]);
-        levelToLoad = (int)level - 47;
-        Debug.Log(level);
-        Debug.Log(levelToLoad);
+        FinishObj = GameObject.FindGameObjectWithTag("Finish");
     }
-    private void _LevelTransition()
+
+    private void _LevelTransition(int levelToLoad)
     {
         SceneManager.LoadScene("level" + levelToLoad);
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Finish"))
+        if (collision.gameObject.tag == ("Player"))
         {
-            Debug.Log(levelToLoad);
-            _LevelTransition();
+            if (FinishObj.layer == LayerMask.NameToLayer("FinishLevel1"))
+            {
+                SceneManager.LoadScene("Level2");
+            }
+            if (FinishObj.layer == LayerMask.NameToLayer("FinishLevel2"))
+            {
+                SceneManager.LoadScene("Level3");
+            }
+
         }
+        //private void OnTriggerStay2D(Collider2D collision)
+        //{
+        //sceneName = SceneManager.GetActiveScene().name;
+        //var level = ((sceneName)[5]);
+        // Debug.Log(level);
+
+
+        //}
+        // }
     }
 }
